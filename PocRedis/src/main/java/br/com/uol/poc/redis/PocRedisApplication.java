373297c8b2b9f6d1994d052a170de5a28560b290
Thiago dos Santos Hora <vrx_thora@uolinc.com>
2016-01-18 14:24:35 -0200

    Alteração de applicação para usar spring-boot

--- PocRedis/src/main/java/br/com/uol/poc/redis/PocRedisApplication.java
+++ PocRedis/src/main/java/br/com/uol/poc/redis/PocRedisApplication.java
@@ -1,7 +1,6 @@
 package br.com.uol.poc.redis;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,38 +8,22 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 @EnableCaching
 @EnableTransactionManagement
 @EnableScheduling
+@EnableRedisHttpSession
 @SpringBootApplication
-@PropertySource("classpath:/redis.properties")
 public class PocRedisApplication extends SpringBootServletInitializer {
-
-	private final Logger log = Logger.getLogger(this.getClass());
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(this.getClass());
-	}
-
-	@Bean
-	public JedisConnectionFactory jedisConnectionFactory(@Value("${redis.host}") String redisHostName,
-														 @Value("${redis.port}") int redisPort) {
-		AtomicReference<JedisConnectionFactory> factory = new AtomicReference<>();
-        factory.set(new JedisConnectionFactory());
-        factory.get().setHostName(redisHostName);
-		factory.get().setPort(redisPort);
-		factory.get().setUsePool(true);
-		return factory.get();
 	}
 
 	@Bean
